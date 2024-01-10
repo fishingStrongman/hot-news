8eb197eb71eefb0372f41a5c8b67dd249c0fa2cd
张赫 <accounts_654b3fc33f15d5487d87bbbc@mail.teambition.com>
2024-01-10 21:42:43 +0800

    更新 schedule.go

--- app/schedule/schedule.go
+++ app/schedule/schedule.go
@@ -1,7 +1,12 @@
 package schedule
 
-import "hotinfo/app/schedule/bilibili"
-
+import (
+	"hotinfo/app/schedule/bilibili"
+	"hotinfo/app/schedule/hyper"
+	"hotinfo/app/schedule/lol"
+)
 func Run() {
 	bilibili.Run()
+	hyper.Run()
+	lol.Run()
 }
