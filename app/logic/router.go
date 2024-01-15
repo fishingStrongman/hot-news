dc37e81176aa95f78352f1ddcf41ad2507a2991b
fishingStrongman <1297084605@qq.com>
2024-01-15 20:19:21 +0800

    更新代码

--- app/logic/router.go
+++ app/logic/router.go
@@ -2,12 +2,10 @@ package logic
 
 import (
 	"github.com/gin-gonic/gin"
-	"hotinfo/app/middleware"
 )
 
 func Router() {
 	r := gin.Default()
-	r.Use(middleware.Cors())
 	r.GET("/refresh", Select)
 	if err := r.Run(":8080"); err != nil {
 		panic("gin 启动失败！")
